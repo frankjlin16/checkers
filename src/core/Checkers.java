7c41540cf002431aced02f08859cdbb7e9cbf05b
frankjlin16 <flin30@asu.edu>
2022-03-24 01:47:20 -0700

    Initial Commit

---
+++ src/core/Checkers.java
@@ -0,0 +1,16 @@
+package core;
+
+/**
+ * Checkers
+ * 
+ * Checker's game logic
+ * 
+ * @author Frank Lin
+ * @version 1.0 pre-release
+ */
+public class Checkers {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ui/CheckersTextConsole.java
@@ -0,0 +1,14 @@
+package ui;
+
+/**
+ * CheckersTextConsole
+ * 
+ * @author Frank Lin
+ * @version 1.0 pre-release
+ */
+public class CheckersTextConsole {
+
+    public static void main(String[] args) {
+        
+    }
+}
