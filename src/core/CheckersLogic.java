324eefbc4c8acb567b0a229a0407fc505c194aed
frankjlin16 <flin30@asu.edu>
2022-03-25 14:32:14 -0700

    Refactor

--- src/core/CheckersLogic.java
+++ src/core/CheckersLogic.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
  */
 public class CheckersLogic {
     /** 0 is player X's turn. 1 is player O's turn. */
-    private String activePlayers;
+    private String activePlayer;
     /** Stores the current board position for the game. */
     private String[][] currentBoard;
 
@@ -21,7 +21,7 @@ public class CheckersLogic {
      * Active player is set to X and board starting position is set.
      */
     public CheckersLogic() {
-        activePlayers = "X";
+        activePlayer = "X";
         currentBoard = new String[][] {
                 { "_", "X", "_", "X", "_", "X", "_", "X" },
                 { "X", "_", "X", "_", "X", "_", "X", "_" },
@@ -39,10 +39,10 @@ public class CheckersLogic {
     }
 
     private void changeActivePlayer() {
-        if (activePlayers == "X")
-            activePlayers = "O";
-        else if (activePlayers == "O")
-            activePlayers = "X";
+        if (activePlayer == "X")
+            activePlayer = "O";
+        else if (activePlayer == "O")
+            activePlayer = "X";
     }
 
     public void updateBoard(int oldRow, int oldCol, int newRow, int newCol) {
@@ -50,16 +50,22 @@ public class CheckersLogic {
         currentBoard[oldRow][oldCol] = "_";
 
         // Update new position
-        currentBoard[newRow][newCol] = activePlayers;
+        currentBoard[newRow][newCol] = activePlayer;
 
         // Change active players
         changeActivePlayer();
     }
 
     public boolean moreMoves() {
-        if (activePlayers == "X") {
+        // If current player is X, search each row for X. If X is not found, move to the next row. If X is found, return true.
+        if (activePlayer == "X") {
             for (String[] row : currentBoard) {
                 if (Arrays.asList(row).contains("X"))
+                    return true;
+            }
+        } else if (activePlayer == "O") {
+            for (String[] row : currentBoard) {
+                if (Arrays.asList(row).contains("O"))
                     return true;
             }
         }
@@ -69,5 +75,9 @@ public class CheckersLogic {
 
     public String[][] getBoard() {
         return currentBoard;
+    }
+
+    public String getActivePlayer() {
+        return activePlayer;
     }
 }
