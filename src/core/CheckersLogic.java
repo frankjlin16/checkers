193283130ad87d42096c6b900cf74048a4519b35
frankjlin16 <flin30@asu.edu>
2022-03-25 00:33:59 -0700

    Add basic methods

--- src/core/CheckersLogic.java
+++ src/core/CheckersLogic.java
@@ -9,8 +9,42 @@ package core;
  * @version 1.0 pre-release
  */
 public class CheckersLogic {
+    /** 0 is player X's turn. 1 is player O's turn. */
+    private char activePlayers;
+    /** Stores the current board position for the game. */
+    private String[][] currentBoard;
 
-    public static void main(String[] args) {
+    /**
+     * Initializes the game logic.
+     * Active player is set to X and board starting position is set.
+     */
+    public CheckersLogic() {
+        activePlayers = 'X';
+        currentBoard = new String[][] {
+                { "_", "X", "_", "X", "_", "X", "_", "X" },
+                { "X", "_", "X", "_", "X", "_", "X", "_" },
+                { "_", "X", "_", "X", "_", "X", "_", "X" },
+                { "_", "_", "_", "_", "_", "_", "_", "_" },
+                { "_", "_", "_", "_", "_", "_", "_", "_" },
+                { "O", "_", "O", "_", "O", "_", "O", "_" },
+                { "_", "O", "_", "O", "_", "O", "_", "O" },
+                { "O", "_", "O", "_", "O", "_", "O", "_" },
+        };
+    }
+
+    public boolean isValid() {
+        return true;
+    }
+
+    private void changeActivePlayer() {
 
     }
+
+    public void updateBoard() {}
+    
+    private boolean gridOccupiedByOpponent() { return false; }
+
+    public boolean moreMoves() { return true; }
+
+    public String[][] getBoard() { return currentBoard; }
 }
--- src/ui/CheckersTextConsole.java
+++ src/ui/CheckersTextConsole.java
@@ -1,5 +1,7 @@
 package ui;
 
+import core.CheckersLogic;
+
 /**
  * CheckersTextConsole
  * 
@@ -8,7 +10,34 @@ package ui;
  */
 public class CheckersTextConsole {
 
+    private static final String rowIndex[] = {"8", "7", "6", "5","4", "3", "2", "1"};
+    private static final String columnIndex[] = {"a", "b", "c", "d","e", "f","g", "h"};
+
+    /** Game driver */
     public static void main(String[] args) {
-        
+        CheckersLogic game = new CheckersLogic();
+        printBoard(game.getBoard());
+    }
+
+    /** Prints current state of the game
+     * 
+     * @param board
+     */
+    private static void printBoard(String[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(rowIndex[i]);
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(" | " + board[i][j]);
+            }
+            System.out.println(" |\n");
+        }
+    }
+
+    private static void requestMove() {
+
+    }
+
+    private static void printError() {
+
     }
 }
