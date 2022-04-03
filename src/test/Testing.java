fbf1a10188cef94cf944d78cb7f5d8682e876df2
frankjlin16 <flin30@asu.edu>
2022-04-02 22:33:30 -0700

    Modify testing

--- src/test/Testing.java
+++ src/test/Testing.java
@@ -1,23 +1,16 @@
 package test;
 
 import core.CheckersComputerPlayer;
+import core.CheckersLogic;
 
 public class Testing {
 
-    static String[][] currentBoard = new String[][] {
-        { "_", "O", "_", "O", "_", "O", "_", "O" },
-        { "O", "_", "O", "_", "O", "_", "O", "_" },
-        { "_", "O", "_", "O", "_", "O", "_", "O" },
-        { "_", "_", "_", "_", "_", "_", "_", "_" },
-        { "_", "_", "_", "_", "_", "_", "_", "_" },
-        { "X", "_", "X", "_", "X", "_", "X", "_" },
-        { "_", "X", "_", "X", "_", "X", "_", "X" },
-        { "X", "_", "X", "_", "X", "_", "X", "_" },
-};
-
     public static void main(String[] args) {
-        CheckersComputerPlayer player = new CheckersComputerPlayer();
-        player.searchMovablePieces(currentBoard);
+        CheckersLogic game = new CheckersLogic();
+        CheckersComputerPlayer player = new CheckersComputerPlayer(game);
+
+        game.changeActivePlayer();
+        player.searchMovablePieces(game.getBoard());
 
     }
     
