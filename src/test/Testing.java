0c974fbfe891df8ac3cc21d69b730b5eaf08298d
frankjlin16 <flin30@asu.edu>
2022-04-02 21:39:16 -0700

    Add test for computer player

--- src/test/Testing.java
+++ src/test/Testing.java
@@ -1,5 +1,24 @@
 package test;
 
+import core.CheckersComputerPlayer;
+
 public class Testing {
+
+    static String[][] currentBoard = new String[][] {
+        { "_", "O", "_", "O", "_", "O", "_", "O" },
+        { "O", "_", "O", "_", "O", "_", "O", "_" },
+        { "_", "O", "_", "O", "_", "O", "_", "O" },
+        { "_", "_", "_", "_", "_", "_", "_", "_" },
+        { "_", "_", "_", "_", "_", "_", "_", "_" },
+        { "X", "_", "X", "_", "X", "_", "X", "_" },
+        { "_", "X", "_", "X", "_", "X", "_", "X" },
+        { "X", "_", "X", "_", "X", "_", "X", "_" },
+};
+
+    public static void main(String[] args) {
+        CheckersComputerPlayer player = new CheckersComputerPlayer();
+        player.searchMovablePieces(currentBoard);
+
+    }
     
 }
