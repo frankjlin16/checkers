c8f517e4809f56355d59feee62939878deedff0c
frankjlin16 <flin30@asu.edu>
2022-04-11 21:01:57 -0700

    Modify checker move

--- src/ui/CheckersGUI.java
+++ src/ui/CheckersGUI.java
@@ -10,12 +10,16 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 public class CheckersGUI extends Application {
 
+    /** The game logic */
     CheckersLogic game = new CheckersLogic();
+
+    Circle prevCircle;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -24,6 +28,9 @@ public class CheckersGUI extends Application {
 
         stage.setTitle("Checkers");
         stage.setScene(scene);
+        stage.show();
+
+        scene = new Scene(showBoard());
         stage.show();
     }
 
@@ -51,16 +58,43 @@ public class CheckersGUI extends Application {
                 } else {
                     color = Color.TRANSPARENT;
                 }
-
                 // Create the circle that represents the checker
                 Circle circle = new Circle(37, color);
                 // Handles mouse clicks on the checker piece
                 EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent e) {
-                        circle.setStroke(Color.YELLOW);
-                        circle.setStrokeWidth(5);
-                        circle.setStrokeType(StrokeType.INSIDE);
+                        if (prevCircle != null) {
+                            if (circle.getFill() == prevCircle.getFill()) {
+                                prevCircle.setStroke(Color.TRANSPARENT);
+                                circle.setStroke(Color.YELLOW);
+                                circle.setStrokeWidth(5);
+                                circle.setStrokeType(StrokeType.INSIDE);
+                                prevCircle = circle;
+                            } else if (circle.getFill() != prevCircle.getFill()) {
+                                int prevRow = GridPane.getRowIndex(prevCircle);
+                                int prevCol = GridPane.getColumnIndex(prevCircle);
+                                int currRow = GridPane.getRowIndex(circle);
+                                int currCol = GridPane.getColumnIndex(circle);
+                                System.out.println(prevRow + " " + prevCol + " " + currRow + " " + currCol);
+
+                                System.out.println(game.isValid(prevRow, prevCol, currRow, currCol));
+                                // Check and make move
+                                if (game.isValid(prevRow, prevCol, currRow, currCol)) {
+                                    game.updateBoard(prevRow, prevCol, currRow, currCol);
+                                    circle.setFill(prevCircle.getFill());
+                                    prevCircle.setFill(Color.TRANSPARENT);
+                                }
+                                // Clean up
+                                prevCircle.setStroke(Color.TRANSPARENT);
+                                prevCircle = null;
+                            }
+                        } else if (circle.getFill() != Color.TRANSPARENT) {
+                            circle.setStroke(Color.YELLOW);
+                            circle.setStrokeWidth(5);
+                            circle.setStrokeType(StrokeType.INSIDE);
+                            prevCircle = circle;
+                        }
                     }
                 };
                 circle.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
